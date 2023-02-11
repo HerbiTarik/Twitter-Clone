edf5b71d55f04aeaa552274448d0abbeebab3c73
Herbi Tarik <herbitarik2509@gmail.com>
2023-02-11 16:48:15 +0100

    complete the signin page

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-v1-f0d90.firebaseapp.com",
   projectId: "twitter-v1-f0d90",
   storageBucket: "twitter-v1-f0d90.appspot.com",
---
+++ pages/auth/Signin.js
@@ -0,0 +1,28 @@
+import {getProviders, signIn } from "next-auth/react";
+export default function Signin({providers}) {
+  return (
+    <div className="flex justify-center mt-20 space-x-4">
+        <img src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-02/ch-2-7-app-store.png.twimg.1920.png" alt="twitter image inside a phone" className="hidden object-cover md:w-44 md:h-80 rotate-6 md:inline-flex"/>
+        <div className="">
+            {Object.values(providers).map((provider) => (
+                <div className="flex flex-col items-center">
+                    <img className="object-cover w-36 " src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png" alt="twitter logo" />
+                    <p className="my-10 text-sm italic text-center ">This app is created for learning purposes</p>
+                    <button onClick={() => signIn(provider.id, {callbackUrl: "/"})} className="p-3 text-white bg-red-400 rounded-lg hover:bg-red-500">Sign in with {provider.name}</button>
+                </div>
+            ))}
+        </div>
+    
+    </div>
+  )
+}
+
+
+export async function getServerSideProps(){
+    const providers = await getProviders();
+    return {
+        props:{
+            providers,
+        }
+    }
+}
