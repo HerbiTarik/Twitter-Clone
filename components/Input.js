8fb005941b67d02e49195b8dd27da92a66e3f145
Herbi Tarik <herbitarik2509@gmail.com>
2023-02-12 11:05:46 +0100

    get the session and modify sidebar and input components

--- components/Input.js
+++ components/Input.js
@@ -1,19 +1,22 @@
 import {FaceSmileIcon, PhotoIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import tarik from '../public/tarik.png'
+import {useSession, signOut} from "next-auth/react"
 
 export default function Input() {
-  
-  console.log(tarik)
+  const {data: session} = useSession();
   return (
-    <div className='flex border-b border-gray-200 p-3 space-x-3 '>
-      <Image 
-      src={tarik}
+    <>
+    {session && (
+      <div className='flex border-b border-gray-200 p-3 space-x-3 '>
+      <img
+      onClick={signOut}
+      src={session.user.image}
       alt="picture of tarik"
       className='h-11 w-11 rounded-full cursor-pointer hover:brightness-95'
       />
       <div className="w-full divide-y divide-gray-200">
-        <div className="">
+      <div className="">
           <textarea className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="what's happening?"></textarea>
         </div>
         <div className="flex items-center justify-between pt-2.5">
@@ -23,8 +26,10 @@ export default function Input() {
           </div>
           <button className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'>Tweet</button>
         </div>
-      </div>
-    
-    </div>
+        </div>
+        
+        </div>
+        )}
+        </>
   )
 }
